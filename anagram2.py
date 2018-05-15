8e706800c29dbcbcdb1baa73213aced5abb7d17a
Talitha Veith <tveith@talithas-air-2.localdomain>
2018-05-15 18:06:15 +0200

    Body of function made more concise

---
+++ anagram2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+def anagram(word1, word2):
+
+    if sorted(word1.lower()) == sorted(word2.lower()):
+	    return True
+
+print("Are these words anagrams of each other?")
+while True:
+	input1 = raw_input("Please enter the first word: ")
+	input2 = raw_input("Please enter the second word: ")
+	if input1.isalpha() and input2.isalpha():
+		if anagram(input1, input2):
+			print("Yes, %s and %s are anagrams of each other" % (input1, input2))
+		else:
+			print("No, %s and %s are not anagrams of each other" % (input1, input2))
+	else:
+		print("Please enter a word containing only letters")
+	again = raw_input("Do you want to quit? y/n --> ")
+	if again == "y":
+		break
