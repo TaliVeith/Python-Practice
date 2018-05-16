e198ed9cd089d8c90529ffa5bcefb91015b6832a
Talitha Veith <tveith@talithas-air-2.localdomain>
2018-05-16 14:35:20 +0200

    new folder structure

---
+++ anagram/anagram2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+def anagram(word1, word2):
+
+    return sorted(word1.lower()) == sorted(word2.lower())
+
+
+print("Are these words anagrams of each other?")
+while True:
+	input1 = raw_input("Please enter the first word: ")
+	input2 = raw_input("Please enter the second word: ")
+	if input1.isalpha() and input2.isalpha():
+		if anagram(input1, input2):
+			print("Yes, %s and %s are anagrams of each other" % (input1, input2))
+		else:
+			print("No, %s and %s are not anagrams of each other" % (input1, input2))
+	else:
+		print("Please enter a word containing only letters")
+	again = raw_input("Do you want to quit? y/n --> ")
+	if again == "y":
+		break
